cbc3a72b2e8f412f29807df6f89658851c4003f9
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-11-09 21:18:49 +0100

    create color constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+const Colors = {
+  green: "#22D486",
+  greenSecondary: "#20BD78",
+  violetRed: "#FF4081",
+  violetRedSecondary: "#E73370",
+  grey: "#D9DCE1",
+  greySecondary: "#C4C9D1",
+  greyText: "#949EA8",
+  lightGrey: "#DAE1E7",
+  mediumGrey: "#C9CED3",
+  mediumGreySecondary: "#7D7878;",
+  darkGrey: "#323C46",
+  darkGreySecondary: "#565D5A",
+  offWhite: " #F2F2F2",
+};
+
+export default Colors;
