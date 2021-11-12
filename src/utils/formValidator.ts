0ed237981783172f869064551dd67e533f3504b0
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-11-12 04:43:58 +0100

    create email validator

---
+++ src/utils/formValidator.ts
@@ -0,0 +1,5 @@
+export const validateEmailInput = (email: string) => {
+  // eslint-disable-next-line
+  const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return EMAIL_PATTERN.test(email.trim());
+};
